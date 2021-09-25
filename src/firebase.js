e8a57f0e492e0dbd6bb485e8ccdc2707541591af
farish <developer.websites1@gmail.com>
2021-09-25 15:17:01 +0530

    connecting project to firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+
+const firebaseApp = firebase.initializeApp()[{
+  apiKey: "AIzaSyBlEHRnkKo3aA2TTeH06CLqct8yXFYcVuA",
+  authDomain: "ringer-chat-app-dbd9b.firebaseapp.com",
+  projectId: "ringer-chat-app-dbd9b",
+  storageBucket: "ringer-chat-app-dbd9b.appspot.com",
+  messagingSenderId: "369723615893",
+  appId: "1:369723615893:web:ac862ec67814bfe012899c",
+  measurementId: "G-SNMGS8X59K"
+}]
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth}
+
